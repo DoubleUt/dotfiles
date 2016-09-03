0f140c0524ccc7cb18bdf99b18c8c0a85870a004
wtnbass <bass.ksk@gmail.com>
2016-09-03 21:08:04 +0900

    setpu.sh: add .tmux.conf

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-DOT_FILES=(.vimrc .zshrc .emacs.d)
+DOT_FILES=(.tmux.conf .vimrc .zshrc .emacs.d)
 
 for file in ${DOT_FILES[@]}
 do
-  ln -s ~/dotfiles/$file ~/$file
+    ln -s ~/dotfiles/$file ~/$file
 done
