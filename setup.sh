7843ac7617878058c86587844c2f760dd0f592e4
wtnbass <wtnbass@icloud.com>
2020-10-17 14:23:02 +0900

    Add npmrc

--- setup.sh
+++ setup.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-DOT_FILES=(.tmux.conf .emacs.d .vimrc .zshrc .zshenv .bashrc .gitconfig)
-function link() {
-    for file in ${DOT_FILES[@]}
-    do
-        ln -Fs $(pwd)/$file ~/$file
-    done
-}
+DOT_FILES=(.tmux.conf .emacs.d .vimrc .zshrc .zshenv .bashrc .gitconfig .npmrc)
 
+for file in ${DOT_FILES[@]}
+do
+    ln -Fs $(pwd)/$file ~/$file
+done
+
+# setup dein for vim
 DEIN_DIR=~/.cache/dein
-function setup_vim() {
-    if [ ! -d $DEIN_DIR ]; then
-        curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-        sh ./installer.sh ~/.cache/dein
-        rm ./installer.sh
-    fi
-}
-
-link
+if [ ! -d $DEIN_DIR ]; then
+    curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+    sh ./installer.sh ~/.cache/dein
+    rm ./installer.sh
+fi
